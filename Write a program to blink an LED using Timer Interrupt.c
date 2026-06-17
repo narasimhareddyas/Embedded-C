3679be8d12f72499d5758ad2caea040d3a116ad7
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-06-17 16:53:22 +0530

    Add files via upload

---
+++ Write a program to blink an LED using Timer Interrupt.c
@@ -0,0 +1,37 @@
+//Write a program to blink an LED using Timer Interrupt.
+
+#include<reg51.h>
+sbit led= P2^0;
+
+unsigned int count = 0;
+
+// Timer0 ISR
+void timer0_ISR(void) interrupt 1
+{
+    TH0 = 0x4C;     
+    TL0 = 0x00;
+
+    count++;
+
+    if(count >= 20) 
+    {
+        led = !led;  
+        count = 0;
+    }
+}
+
+void main()
+{
+    TMOD = 0x01;  
+
+    TH0 = 0x4C;      
+    TL0 = 0x00;
+
+    IE = 0x82;       
+
+    TR0 = 1;         
+while(1)
+{
+
+}
+}
