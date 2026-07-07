16b835a70ef012b9c894204a3c266ab5119c2f14
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-07-07 21:22:43 +0530

    Add files via upload

---
+++ Write a program to save switch status in EEPROM.c
@@ -0,0 +1,102 @@
+// Write a program to save switch status in EEPROM
+
+#include <reg51.h>
+
+sbit SDA = P2^0;
+sbit SCL = P2^1;
+
+sbit SW = P1^0;
+
+void delay(void);
+void i2c_start(void);
+void i2c_stop(void);
+void i2c_write(unsigned char ch);
+void eeprom_write(unsigned char addr, unsigned char value);
+
+// Delay Function
+void delay(void)
+{
+    unsigned int i;
+
+    for(i = 0; i < 500; i++);
+}
+
+void i2c_start(void)
+{
+    SDA = 1;
+    SCL = 1;
+    delay();
+
+    SDA = 0;
+    delay();
+
+    SCL = 0;
+}
+
+void i2c_stop(void)
+{
+    SDA = 0;
+    SCL = 1;
+    delay();
+
+    SDA = 1;
+    delay();
+}
+
+void i2c_write(unsigned char ch)
+{
+    unsigned char i;
+
+    for(i = 0; i < 8; i++)
+    {
+        if(ch & 0x80)
+            SDA = 1;
+        else
+            SDA = 0;
+
+        SCL = 1;
+        delay();
+
+        SCL = 0;
+        delay();
+
+        ch <<= 1;
+    }
+
+    SDA = 1;
+    SCL = 1;
+    delay();
+    SCL = 0;
+}
+
+void eeprom_write(unsigned char addr, unsigned char value)
+{
+    i2c_start();
+
+    i2c_write(0xA0);      
+
+    i2c_write(addr);     
+
+    i2c_write(value);     
+
+    i2c_stop();
+
+    delay();              
+}
+
+void main(void)
+{
+    while(1)
+    {
+        if(SW == 0)                  
+        {
+            eeprom_write(0x00, 0x01); 
+        }
+        else                        
+        {
+            eeprom_write(0x00, 0x00); 
+        }
+
+        delay();
+    }
+}
