0612d553879268fce2788e1694449b4d2342af06
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-07-16 20:07:30 +0530

    Add files via upload

---
+++ Write a program to rotate DC motor clockwise.c
@@ -0,0 +1,29 @@
+// Write a program to rotate DC motor clockwise
+
+#include <reg51.h>
+
+sbit IN1 = P2^0;
+sbit IN2 = P2^1;
+sbit EN  = P2^2;
+
+void delay(void)
+{
+    unsigned int i, j;
+
+    for(i = 0; i < 500; i++)
+    {
+        for(j = 0; j < 300; j++);
+    }
+}
+
+void main(void)
+{
+    EN = 1;         
+    while(1)
+    {
+        IN1 = 1;     
+        IN2 = 0;
+
+        delay();
+    }
+}
