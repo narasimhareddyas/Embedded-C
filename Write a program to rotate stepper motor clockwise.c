96461bef14d87635a163631e3cf1a64040ba6171
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-07-21 19:24:50 +0530

    Add files via upload

---
+++ Write a program to rotate stepper motor clockwise.c
@@ -0,0 +1,31 @@
+// Write a program to rotate stepper motor clockwise
+
+#include <reg51.h>
+
+void delay(void)
+{
+    unsigned int i, j;
+
+    for(i = 0; i < 200; i++)
+    {
+        for(j = 0; j < 300; j++);
+    }
+}
+
+void main(void)
+{
+    while(1)
+    {
+        P2 = 0x09;     
+        delay();
+
+        P2 = 0x0C;    
+        delay();
+
+        P2 = 0x06;     
+        delay();
+
+        P2 = 0x03;   
+        delay();
+    }
+}
